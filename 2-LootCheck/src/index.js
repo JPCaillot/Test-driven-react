4d3be02652360cc289c9bcecaacb6808786563d4
JPCaillot <joaopcaillot@gmail.com>
2023-03-17 10:24:50 -0300

    Finished

--- 2-LootCheck/src/index.js
+++ 2-LootCheck/src/index.js
@@ -7,6 +7,7 @@ import rootReducer from "./reducers";
 import { Provider } from "react-redux";
 
 import App from "./components/App";
+import './index.css';
 
 const store = createStore(rootReducer, applyMiddleware(thunk));
 
